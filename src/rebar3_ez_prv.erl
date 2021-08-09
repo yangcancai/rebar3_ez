de4f2944980524367cd0f0ecda2ddc3e75c111db
admin <admin.com>
2021-08-09 15:05:13 +0800

    Init

--- src/rebar3_ez_prv.erl
+++ src/rebar3_ez_prv.erl
@@ -77,6 +77,7 @@ tar_ez([#{ebin_dir := _EbinDir,
     {ok, Repo} = file:get_cwd(),
     ok = file:set_cwd(OutDir),
     {ok, _} = zip:create(TarName, Fs1),
+    ok = file:rename(TarName, filename:join(PluginsDir, TarName)),
     ok = file:set_cwd(Repo),
     tar_ez(Rest, PluginsDir, State).
 
